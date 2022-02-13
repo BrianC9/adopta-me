77cf8a66f8c96427eb66a003e1ce2ef7ffb872d5
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-02-13 19:48:12 +0100

    Configuracion de Parcel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
 //Para ignorar el linter y decirle que React es un elemento global
 
-/*global React ReactDOM */
 // Props, propiedaes de un componente. Se pasan como un objeto
 // El flujo de información en React es Vertical de padre a hijo, nunca podremos modificar el componente padre, por ello le pasamos unas propiedades que únicamente alteran a la instancia de nuestro componente
 const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.nombre),
-    React.createElement("h4", {}, props.animal),
-    React.createElement("h4", {}, props.raza),
+    React.createElement("h1", {}, props.nombre),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.raza),
   ]);
 };
 
