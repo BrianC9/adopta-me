02d16b9a73ddd763ecf32e7a7bfb7b28dff76096
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-02-13 19:29:27 +0100

    Configuracion del linter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+//Para ignorar el linter y decirle que React es un elemento global
+
+/*global React ReactDOM */
 // Props, propiedaes de un componente. Se pasan como un objeto
 // El flujo de información en React es Vertical de padre a hijo, nunca podremos modificar el componente padre, por ello le pasamos unas propiedades que únicamente alteran a la instancia de nuestro componente
 const Pet = (props) => {
